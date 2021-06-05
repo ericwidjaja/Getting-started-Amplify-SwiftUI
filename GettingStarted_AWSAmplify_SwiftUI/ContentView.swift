1f0305a4fc61aff819e4eeef0b3ea982b443f102
Eric Widjaja <ericwidjaja@pursuit.org>
2021-06-04 20:49:50 -0400

    intial commit

---
+++ GettingStarted_AWSAmplify_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GettingStarted_AWSAmplify_SwiftUI
+//
+//  Created by Eric Widjaja on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
